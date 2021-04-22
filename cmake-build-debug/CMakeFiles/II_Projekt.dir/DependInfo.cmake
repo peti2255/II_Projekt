75dc2a5295411fd56ea84f4737cb73de007060ab
peti2255 <48029773+peti2255@users.noreply.github.com>
2021-04-22 18:25:11 +0300

    Elso

---
+++ cmake-build-debug/CMakeFiles/II_Projekt.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/Egyetem/II_Projekt/main.c" "D:/Egyetem/II_Projekt/cmake-build-debug/CMakeFiles/II_Projekt.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/II_Projekt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/II_Projekt.dir/main.c.obj"
+  "II_Projekt.exe"
+  "II_Projekt.exe.manifest"
+  "II_Projekt.pdb"
+  "libII_Projekt.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/II_Projekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
